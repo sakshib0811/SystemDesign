283bff75f2f86b9b1105cdbb65dc77d8e720910e
sakshib0811 <85578492+sakshib0811@users.noreply.github.com>
2026-03-14 02:48:15 +0530

    Modified UserRepo

--- FoodOrderingSystem/Repository/UserRepository.java
+++ FoodOrderingSystem/Repository/UserRepository.java
@@ -5,15 +5,7 @@ import java.util.*;
 import Models.User;
 
 public class UserRepository {
-    private static UserRepository instance = null;
     private Map<String, User> users = new HashMap<>();
-
-    public static UserRepository getInstance() {
-        if (instance != null)
-            return instance;
-        instance = new UserRepository();
-        return instance;
-    }
 
     // save method helps to register new users
     public void save(User user) {
